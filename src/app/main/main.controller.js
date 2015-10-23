d0c6a8ef372fb21867a6d8b12730eb036abbbb93
dracostheblack <dracostheblack@gmail.com>
2015-10-23 16:40:12 -0400

    added the nav  bar

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -6,13 +6,89 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($scope,$timeout, webDevTec, toastr,$log) {
+  function MainController($scope, $timeout, webDevTec, toastr, $log) {
 
     var vm = this;
+    var gmap,trailLayer, hazzardLayer, entertainmentLayer;
 
-    $scope.$on('mapInitialized', function(event, map) {
-        google.maps.event.trigger(map, "resize");
+    var layers = {
+      trail: {
+        url: 'https://raw.githubusercontent.com/bythorsbeard/etrails/master/src/assets/kml/ElizabethRiverTrail.kml',
+        suppressInfoWindows: true,
+        preserveViewport: true
+      },
+      hazards: {
+        url: 'https://raw.githubusercontent.com/bythorsbeard/etrails/master/src/assets/kml/Hazards.kml',
+        suppressInfoWindows: true,
+        preserveViewport: true
+      },
+      entertainment: {
+        url: 'https://raw.githubusercontent.com/bythorsbeard/etrails/master/src/assets/kml/Entertainment.kml',
+        suppressInfoWindows: true,
+        preserveViewport: true
+      }
+
+
+    }
+
+    $scope.$on('mapInitialized', function (event, map) {
+      //google.maps.event.trigger(map, "resize");
+      gmap = map;
+      trailLayer = new google.maps.KmlLayer(layers.trail);
+      trailLayer.setMap(map);
+
+      hazzardLayer = new google.maps.KmlLayer(layers.hazards);
+      hazzardLayer.setMap(map);
+
+      entertainmentLayer = new google.maps.KmlLayer(layers.entertainment);
+      entertainmentLayer.setMap(map);
+
+      var infoWindow = new google.maps.InfoWindow({map: map});
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          log.info(pos);
+          map.setCenter(pos);
+        }, function(test) {
+          $log.error(test);
+          $log.error("wtf");
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        $log.error("huh");
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+
+
+      $log.info(map);
     });
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    }
+
+    $scope.hideLayer = function () {
+      if(hazzardLayer.getMap()) {
+        hazzardLayer.setMap(null);
+      }else{
+        hazzardLayer = new google.maps.KmlLayer(layers.hazards);
+        hazzardLayer.setMap(gmap);
+      }
+
+    };
+
 
     vm.awesomeThings = [];
     vm.classAnimation = '';
@@ -23,7 +99,7 @@
 
     function activate() {
       getWebDevTec();
-      $timeout(function() {
+      $timeout(function () {
         vm.classAnimation = 'rubberBand';
       }, 4000);
     }
@@ -36,7 +112,7 @@
     function getWebDevTec() {
       vm.awesomeThings = webDevTec.getTec();
 
-      angular.forEach(vm.awesomeThings, function(awesomeThing) {
+      angular.forEach(vm.awesomeThings, function (awesomeThing) {
         awesomeThing.rank = Math.random();
       });
     }
